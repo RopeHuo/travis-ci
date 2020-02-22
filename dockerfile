6bd2f158063cfe19fb051d83171fdbe94ab0993e
Rope <scarf666@163.com>
2020-02-22 21:52:24 +0800

    update

--- dockerfile
+++ dockerfile
@@ -3,6 +3,4 @@ COPY . /app
 WORKDIR /app
 RUN npm install
 EXPOSE 3000
-CMD node ./hello.js
-
-
+CMD node hello.js
--- hello.js
+++ hello.js
@@ -4,4 +4,7 @@ const app = new Koa();
 app.use((ctx, next) => {
     ctx.response.body = 'hello world'
 })
+module.exports = function(){
+	return 'Hello Node';
+}
 app.listen(3000);
