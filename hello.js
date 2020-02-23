3510bae2a509f46fc47ec4d8c655da55cef36d2e
Rope <scarf666@163.com>
2020-02-23 17:10:23 +0800

    update test

--- hello.js
+++ hello.js
@@ -4,7 +4,8 @@ const app = new Koa();
 app.use((ctx, next) => {
     ctx.response.body = 'hello docker'
 })
-module.exports = function(){
-	return 'Hello Node';
-}
+// 这里是学习测试功能的代码
+// module.exports = function hello(){
+// 	return 'Hello Node';
+// };
 app.listen(3000);
